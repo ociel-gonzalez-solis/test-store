c4499286c71e553ac51e3d36f21ddc50448379eb
ociel-gonzalez-solis <ocielgonzalezsolis@hotmail.com>
2023-11-13 22:32:55 -0800

    Next Auth login.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,19 @@
-import NextAuth from "next-auth"
+import { dbUsers } from "@/database";
+import NextAuth, { NextAuthOptions } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import GithubProvider from "next-auth/providers/github"
 
-export const authOptions = {
+declare module "next-auth" {
+    interface Session {
+        accessToken?: string;
+    }
+    interface User {
+        id?: string
+        _id: string
+    }
+};
+
+export const authOptions: NextAuthOptions = {
     // Configure one or more authentication providers
     providers: [
         GithubProvider({
@@ -19,16 +30,47 @@ export const authOptions = {
             async authorize(credentials){
                 console.log({credentials});
 
-                return {id: '123', name: 'Juan', email: 'juan@google.com', role: 'admin'};
+                // return {_id: '123', name: 'Juan', email: 'juan@google.com', role: 'admin'};
+
+                return await dbUsers.checkUserEmailPassword(
+                    credentials!.email, credentials!.password
+                )
             }
         })
-    ],
-    jwt: {
-
-    },
+    ], 
+    
+    // jwt() {
+    //     return null;
+    // },
 
     callbacks: {
-        
+        async jwt({token, account, user}) {
+            console.log({ token, account, user });
+
+            if(account){
+                token.accessToken = account.access_token;
+
+                switch(account.type){
+                    case 'oauth':
+                        // TODO: Crear usuarioso verificar que existan en la bd
+                    break;
+
+                    case 'credentials':
+                        token.user = user;
+                    break;
+                }
+            }
+
+            return token;
+        },
+        async session({ session, token, user }){
+            console.log({session, token, user });
+
+            session.accessToken = token.accessToken as any;
+            session.user        = token.user as any;
+
+            return session;
+        }
     }
 }
 
